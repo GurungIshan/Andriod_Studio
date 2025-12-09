8b8976ae020a1e71f1de3fed04dc05b0802c25d2
ishangrg66 <ishangurung187@gmail.com>
2025-12-09 07:49:07 +0545

    Dialog update

--- dialog_example/app/src/main/java/np/edu/kathford/dialog_example/MainActivity.java
+++ dialog_example/app/src/main/java/np/edu/kathford/dialog_example/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -33,6 +35,38 @@ public class MainActivity extends AppCompatActivity {
                 logoutDialogFragment.show(getSupportFragmentManager(), "LogoutFragmentManager");
             }
         });
+        customDialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                customDialog();
+            }
+        });
+    }
+    private void customDialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.dialog_simple_interest, null);
+
+        EditText principalEditText = view.findViewById(R.id.principalEditText);
+        EditText timeEditText = view.findViewById(R.id.timeEditText);
+        EditText rateEditText = view.findViewById(R.id.rateEditText);
+        Button calculateButton = view.findViewById(R.id.calculateButton);
+        TextView resultTextView = view.findViewById(R.id.resultTextView);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double principal = Double.parseDouble(principalEditText.getText().toString().trim());
+                double rate = Double.parseDouble(rateEditText.getText().toString().trim());
+                int time = Integer.parseInt(timeEditText.getText().toString().trim());
+
+                double SI = (principal*time*rate)/100;
+
+                resultTextView.setText("The SI is: "+SI);
+            }
+        });
+        builder.setView(view);
+        AlertDialog alert = builder.create();
+        alert.show();
     }
     private void logoutButton(){
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
