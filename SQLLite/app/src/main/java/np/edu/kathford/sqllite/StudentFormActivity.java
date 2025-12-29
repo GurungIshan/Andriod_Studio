3323afd2ead26ded48ec51ecf865ce29c9156e34
ishangrg66 <ishangurung187@gmail.com>
2025-12-29 08:27:57 +0545

    SQLite

---
+++ SQLLite/app/src/main/java/np/edu/kathford/sqllite/StudentFormActivity.java
@@ -0,0 +1,4 @@
+package np.edu.kathford.sqllite;
+
+public class StudentFormActivity {
+}
---
+++ SQLLite/app/src/main/java/np/edu/kathford/sqllite/models/Student.java
@@ -0,0 +1,49 @@
+package np.edu.kathford.sqllite.models;
+
+public class Student {
+    private int id;
+    private String fullname;
+
+    public Student(int id, String fullname, String mobileNumber, String address) {
+        this.id = id;
+        this.fullname = fullname;
+        this.mobileNumber = mobileNumber;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    private String mobileNumber;
+    private String address;
+
+}
